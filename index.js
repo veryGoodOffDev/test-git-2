aa6f3f27836d762fbb7ebfed8a236aee67faa353
VeryGoodOff <iluha.kot@gmail.com>
2021-12-23 15:29:25 +0300

    made changes master

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log('Hello World');
 console.error('Error');
 console.log('Helllooooooooooooo')
+
+const userAge = 35;
+if (userAge >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вы не соврешеннолетний')
+}
