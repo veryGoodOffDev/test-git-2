37294c7be963aceff3e8dcd1a4722d6e4797be86
VeryGoodOff <iluha.kot@gmail.com>
2021-12-23 15:26:22 +0300

    made changes in feature/user-access

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//master
+
 console.log('Hello World');
 console.error('Error');
 console.log('Helllooooooooooooo')
+
+
+//develop
+
+console.log('Hello Java Script');
+
+// на одной и той же строчке находится разный код
+
+const age = 20;
+if (age >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вы НЕ совершеннолетний')
+}
