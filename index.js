e61800563c330feba99acca01284f5b2dff9fce4
VeryGoodOff <iluha.kot@gmail.com>
2021-12-23 15:36:44 +0300

    merge feature/user-access in master

--- index.js
+++ index.js
@@ -4,24 +4,9 @@ console.log('Hello World');
 console.error('Error');
 console.log('Helllooooooooooooo')
 
-<<<<<<< HEAD
 const userAge = 35;
 if (userAge >= 18) {
     console.log('Вы совершеннолетний')
 } else {
     console.log('Вы не соврешеннолетний')
-=======
-
-//develop
-
-console.log('Hello Java Script');
-
-// на одной и той же строчке находится разный код
-
-const age = 20;
-if (age >= 18) {
-    console.log('Вы совершеннолетний')
-} else {
-    console.log('Вы НЕ совершеннолетний')
->>>>>>> feature/user-access
 }
